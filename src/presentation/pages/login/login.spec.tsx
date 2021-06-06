2eef2052de905581ccd6d027bacf2e16f8a1eb98
Rodrigo Rodrigues <rodrigo.rsantos@gmail.com>
2021-06-06 15:45:33 -0300

    refactor: breakline block of tests correct

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -47,6 +47,7 @@ describe('Login Component', () => {
 
     const emailInput = sut.getByTestId('email')
     fireEvent.input(emailInput, { target: { value: faker.internet.email() } })
+
     const emailStatus = sut.getByTestId('email-status')
     expect(emailStatus.title).toBe(validationStub.errorMessage)
     expect(emailStatus.textContent).toBe('🔴')
@@ -57,6 +58,7 @@ describe('Login Component', () => {
 
     const passwordInput = sut.getByTestId('password')
     fireEvent.input(passwordInput, { target: { value: faker.internet.password() } })
+
     const passwordStatus = sut.getByTestId('password-status')
     expect(passwordStatus.title).toBe(validationStub.errorMessage)
     expect(passwordStatus.textContent).toBe('🔴')
@@ -66,8 +68,10 @@ describe('Login Component', () => {
     const { sut, validationStub } = makeSut()
 
     validationStub.errorMessage = null
+
     const emailInput = sut.getByTestId('email')
     fireEvent.input(emailInput, { target: { value: faker.internet.email() } })
+
     const emailStatus = sut.getByTestId('email-status')
     expect(emailStatus.title).toBe('Tudo certo!')
     expect(emailStatus.textContent).toBe('🟢')
@@ -77,8 +81,10 @@ describe('Login Component', () => {
     const { sut, validationStub } = makeSut()
 
     validationStub.errorMessage = null
+
     const passwordInput = sut.getByTestId('password')
     fireEvent.input(passwordInput, { target: { value: faker.internet.password() } })
+
     const passwordStatus = sut.getByTestId('password-status')
     expect(passwordStatus.title).toBe('Tudo certo!')
     expect(passwordStatus.textContent).toBe('🟢')
