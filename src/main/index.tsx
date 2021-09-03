0e3119d4d2f50a60a2241ec3ed01228c851421a6
Rodrigo Rodrigues <rodrigo.rsantos@gmail.com>
2021-09-02 21:33:48 -0300

    feat: add SignUp Composition

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -4,10 +4,12 @@ import { Router } from '@/presentation/components'
 
 import '@/presentation/styles/global.scss'
 import { makeLogin } from './factories/pages/login/login-factory'
+import { makeSignUp } from './factories/pages/signup/signup-factory'
 
 ReactDOM.render(
   <Router
     makeLogin={makeLogin}
+    makeSignUp={makeSignUp}
   />,
   document.getElementById('main')
 )
