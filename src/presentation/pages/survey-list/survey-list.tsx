850b28dafd0f5c6e53e0ec104f22582107d06a5f
Rodrigo Rodrigues <rodrigo.rsantos@gmail.com>
2022-03-02 17:52:05 -0300

    refactor: adapt layout to mobile

--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -27,6 +27,38 @@ const SurveyList: React.FC = () => {
 
             <footer>Ver Resultado</footer>
           </li>
+
+          <li>
+            <div className={Styles.surveyContent}>
+              <Icon className={Styles.iconWrap} iconName={IconName.thumbUp} />
+
+              <time>
+                <span className={Styles.day}>01</span>
+                <span className={Styles.month}>03</span>
+                <span className={Styles.year}>2022</span>
+              </time>
+
+              <p>Qual é seu framework web favorito?</p>
+            </div>
+
+            <footer>Ver Resultado</footer>
+          </li>
+
+          <li>
+            <div className={Styles.surveyContent}>
+              <Icon className={Styles.iconWrap} iconName={IconName.thumbUp} />
+
+              <time>
+                <span className={Styles.day}>01</span>
+                <span className={Styles.month}>03</span>
+                <span className={Styles.year}>2022</span>
+              </time>
+
+              <p>Qual é seu framework web favorito?</p>
+            </div>
+
+            <footer>Ver Resultado</footer>
+          </li>
         </ul>
       </div>
 
